07992b9d5d8f7e88a40d6c46e611a4993763efab
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2020-07-22 14:24:35 -0400

    Add results for Coffee Lake.

--- benches/memcpy.rs
+++ benches/memcpy.rs
@@ -17,37 +17,37 @@ use bytesize::ByteSize;
 ///     L3: 16 MiB
 ///
 ///                                 2 kiB
-///                     Zen 2
-/// mov 32         234 ns (100 %)
-/// mov 64          80 ns ( 34 %)
-/// mov 128         63 ns ( 27 %)
-/// mov 256         15 ns (  7 %)
-/// mov 128 (nt)   111 ns ( 47 %)
-/// mov 256 (nt)   111 ns ( 47 %)
-/// rep movsb       29 ns ( 12 %)
-/// rep movsq       29 ns ( 12 %)
+///                     Zen 2               Coffee Lake
+/// mov 32         234 ns (100 %)          135 ns (100 %)
+/// mov 64          80 ns ( 34 %)           89 ns ( 66 %)
+/// mov 128         63 ns ( 27 %)           39 ns ( 29 %)
+/// mov 256         15 ns (  7 %)           20 ns ( 15 %)
+/// mov 128 (nt)   111 ns ( 47 %)           63 ns ( 47 %)
+/// mov 256 (nt)   111 ns ( 47 %)           55 ns ( 41 %)
+/// rep movsb       29 ns ( 12 %)           23 ns ( 17 %)
+/// rep movsq       29 ns ( 12 %)           23 ns ( 17 %)
 ///
 ///                                 4 MiB
-///                     Zen 2
-/// mov 32         328 μs (100 %)
-/// mov 64         168 μs ( 51 %)
-/// mov 128         82 μs ( 25 %)
-/// mov 256         77 μs ( 23 %)
-/// mov 128 (nt)   223 μs ( 68 %)
-/// mov 256 (nt)   224 μs ( 68 %)
-/// rep movsb      340 μs (103 %)
-/// rep movsq      345 μs (105 %)
+///                     Zen 2               Coffee Lake
+/// mov 32         328 μs (100 %)          321 μs (100 %)
+/// mov 64         168 μs ( 51 %)          190 μs ( 59 %)
+/// mov 128         82 μs ( 25 %)          146 μs ( 45 %)
+/// mov 256         77 μs ( 23 %)          166 μs ( 52 %)
+/// mov 128 (nt)   223 μs ( 68 %)          171 μs ( 53 %)
+/// mov 256 (nt)   224 μs ( 68 %)          165 μs ( 51 %)
+/// rep movsb      340 μs (103 %)          163 μs ( 51 %)
+/// rep movsq      345 μs (105 %)          162 μs ( 50 %)
 ///
 ///                                 1 GiB
-///                     Zen 2
-/// mov 32         167 ms (100 %)
-/// mov 64         192 ms (115 %)
-/// mov 128        161 ms ( 96 %)
-/// mov 256        143 ms ( 86 %)
-/// mov 128 (nt)    85 ms ( 51 %)
-/// mov 256 (nt)    82 ms ( 49 %)
-/// rep movsb      163 ms ( 98 %)
-/// rep movsq      163 ms ( 98 %)
+///                     Zen 2               Coffee Lake
+/// mov 32         167 ms (100 %)          141 ms (100 %)
+/// mov 64         192 ms (115 %)          114 ms ( 81 %)
+/// mov 128        161 ms ( 96 %)          106 ms ( 75 %)
+/// mov 256        143 ms ( 86 %)          108 ms ( 77 %)
+/// mov 128 (nt)    85 ms ( 51 %)           80 ms ( 57 %)
+/// mov 256 (nt)    82 ms ( 49 %)          100 ms ( 71 %)
+/// rep movsb      163 ms ( 98 %)          128 ms ( 91 %)
+/// rep movsq      163 ms ( 98 %)          128 ms ( 91 %)
 fn run_benchmark_memcpy(c: &mut Criterion, size: ByteSize) {
     let raw_size = size.as_u64() as usize;
     let mut run_benchmark = |name: &str, memcpy: fn(usize, *mut u8, *mut u8)| {
@@ -62,10 +62,10 @@ fn run_benchmark_memcpy(c: &mut Criterion, size: ByteSize) {
     run_benchmark("mov 64", memcpy::memcpy_mov_64);
     run_benchmark("mov 128", memcpy::memcpy_mov_128);
     #[cfg(target_feature = "avx")]
-    run_benchmark("mov 256", memcpy_mov_256);
+    run_benchmark("mov 256", memcpy::memcpy_mov_256);
     run_benchmark("mov 128 (nt)", memcpy::memcpy_mov_128_nt);
     #[cfg(target_feature = "avx")]
-    run_benchmark("mov 256 (nt)", memcpy_mov_256_nt);
+    run_benchmark("mov 256 (nt)", memcpy::memcpy_mov_256_nt);
     run_benchmark("rep movsb", memcpy::memcpy_rep_movsb);
     run_benchmark("rep movsq", memcpy::memcpy_rep_movsq);
 }
