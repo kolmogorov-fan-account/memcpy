1eda9c6142e93bd34c0210e223d14e488dea2ef6
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2020-07-23 12:37:42 -0400

    Add more non-temporal and pipelined benchmarks.

--- benches/memcpy.rs
+++ benches/memcpy.rs
@@ -1,6 +1,6 @@
+use bytesize::ByteSize;
 use criterion::{criterion_group, criterion_main, Criterion};
 use memmap::MmapMut;
-use bytesize::ByteSize;
 
 /// Zen 2 (AMD)
 ///     Desktop
@@ -17,42 +17,85 @@ use bytesize::ByteSize;
 ///     L3: 16 MiB
 ///
 ///                                 2 kiB
+///
 ///                     Zen 2               Coffee Lake
-/// mov 32         234 ns (100 %)          135 ns (100 %)
-/// mov 64          80 ns ( 34 %)           89 ns ( 66 %)
-/// mov 128         63 ns ( 27 %)           39 ns ( 29 %)
-/// mov 256         15 ns (  7 %)           20 ns ( 15 %)
-/// mov 128 (nt)   111 ns ( 47 %)           63 ns ( 47 %)
-/// mov 256 (nt)   111 ns ( 47 %)           55 ns ( 41 %)
-/// rep movsb       29 ns ( 12 %)           23 ns ( 17 %)
-/// rep movsq       29 ns ( 12 %)           23 ns ( 17 %)
+///
+/// mov 32         234 ns (100 %)          120 ns (100 %)
+///
+/// mov 64          80 ns ( 34 %)           89 ns ( 74 %)
+/// mov 128         63 ns ( 27 %)           37 ns ( 31 %)
+/// mov 256         15 ns (  7 %)           20 ns ( 17 %)
+///
+/// mov 64 (pl)                             58 ns ( 48 %)
+/// mov 128 (pl)                            30 ns ( 25 %)
+/// mov 256 (pl)                            16 ns ( 25 %)
+///
+/// mov 64 (nt)                             81 ns ( 68 %)
+/// mov 128 (nt)   111 ns ( 47 %)           64 ns ( 53 %)
+/// mov 256 (nt)   111 ns ( 47 %)           54 ns ( 45 %)
+///
+/// mov 64 (p+n)                            81 ns ( 49 %)
+/// mov 128 (p+n)  111 ns ( 47 %)           54 ns ( 45 %)
+/// mov 256 (p+n)  111 ns ( 47 %)           54 ns ( 45 %)
+///
+/// rep movsb       29 ns ( 12 %)           22 ns ( 18 %)
+/// rep movsq       29 ns ( 12 %)           23 ns ( 19 %)
 ///
 ///                                 4 MiB
+///
 ///                     Zen 2               Coffee Lake
-/// mov 32         328 μs (100 %)          321 μs (100 %)
-/// mov 64         168 μs ( 51 %)          190 μs ( 59 %)
-/// mov 128         82 μs ( 25 %)          146 μs ( 45 %)
-/// mov 256         77 μs ( 23 %)          166 μs ( 52 %)
-/// mov 128 (nt)   223 μs ( 68 %)          171 μs ( 53 %)
-/// mov 256 (nt)   224 μs ( 68 %)          165 μs ( 51 %)
-/// rep movsb      340 μs (103 %)          163 μs ( 51 %)
-/// rep movsq      345 μs (105 %)          162 μs ( 50 %)
+///
+/// mov 32         328 μs (100 %)          306 μs (100 %)
+///
+/// mov 64         168 μs ( 51 %)          245 μs ( 80 %)
+/// mov 128         82 μs ( 25 %)          146 μs ( 48 %)
+/// mov 256         77 μs ( 23 %)          136 μs ( 44 %)
+///
+/// mov 64 (p)                             177 μs ( 58 %)
+/// mov 128 (p)                            146 µs ( 48 %)
+/// mov 256 (p)                            134 µs ( 44 %)
+///
+/// mov 64 (n)                             173 μs ( 57 %)
+/// mov 128 (n)    223 μs ( 68 %)          173 μs ( 57 %)
+/// mov 256 (n)    224 μs ( 68 %)          168 μs ( 55 %)
+///
+/// mov 64 (p+n)                           197 ns ( 64 %)
+/// mov 128 (p+n)  111 ns ( 47 %)          178 ns ( 58 %)
+/// mov 256 (p+n)  111 ns ( 47 %)          179 ns ( 58 %)
+///
+/// rep movsb      340 μs (103 %)          167 μs ( 55 %)
+/// rep movsq      345 μs (105 %)          211 μs ( 68 %)
 ///
 ///                                 1 GiB
+///
 ///                     Zen 2               Coffee Lake
-/// mov 32         167 ms (100 %)          141 ms (100 %)
-/// mov 64         192 ms (115 %)          114 ms ( 81 %)
-/// mov 128        161 ms ( 96 %)          106 ms ( 75 %)
-/// mov 256        143 ms ( 86 %)          108 ms ( 77 %)
-/// mov 128 (nt)    85 ms ( 51 %)           80 ms ( 57 %)
-/// mov 256 (nt)    82 ms ( 49 %)          100 ms ( 71 %)
-/// rep movsb      163 ms ( 98 %)          128 ms ( 91 %)
-/// rep movsq      163 ms ( 98 %)          128 ms ( 91 %)
+///
+/// mov 32         167 ms (100 %)          145 ms (100 %)
+///
+/// mov 64         192 ms (115 %)          121 ms ( 83 %)
+/// mov 128        161 ms ( 96 %)          106 ms ( 73 %)
+/// mov 256        143 ms ( 86 %)          112 ms ( 77 %)
+///
+/// mov 64 (p)                             115 ms ( 79 %)
+/// mov 128 (p)                            108 ms ( 74 %)
+/// mov 256 (p)                            111 ms ( 77 %)
+///
+/// mov 64 (n)                              87 ms ( 60 %)
+/// mov 128 (n)                             80 ms ( 55 %)
+/// mov 256 (n)                             98 ms ( 68 %)
+///
+/// mov 64 (p+n)                            86 ms ( 59 %)
+/// mov 128 (p+n)                           81 ms ( 56 %)
+/// mov 256 (p+n)                          102 ms ( 70 %)
+///
+/// rep movsb      163 ms ( 98 %)          129 ms ( 89 %)
+/// rep movsq      163 ms ( 98 %)          128 ms ( 88 %)
 fn run_benchmark_memcpy(c: &mut Criterion, size: ByteSize) {
     let raw_size = size.as_u64() as usize;
     let mut run_benchmark = |name: &str, memcpy: fn(usize, *mut u8, *mut u8)| {
         let mut source = MmapMut::map_anon(raw_size).unwrap();
         let mut destination = MmapMut::map_anon(raw_size).unwrap();
+        memcpy(raw_size, source.as_mut_ptr(), destination.as_mut_ptr());
         c.bench_function(&format!("[{}] {}", size.to_string_as(true), name), |b| {
             b.iter(|| memcpy(raw_size, source.as_mut_ptr(), destination.as_mut_ptr()))
         });
@@ -63,9 +106,18 @@ fn run_benchmark_memcpy(c: &mut Criterion, size: ByteSize) {
     run_benchmark("mov 128", memcpy::memcpy_mov_128);
     #[cfg(target_feature = "avx")]
     run_benchmark("mov 256", memcpy::memcpy_mov_256);
+    run_benchmark("mov 64 (pl)", memcpy::memcpy_mov_64_pl);
+    run_benchmark("mov 128 (pl)", memcpy::memcpy_mov_128_pl);
+    #[cfg(target_feature = "avx")]
+    run_benchmark("mov 256 (pl)", memcpy::memcpy_mov_256_pl);
+    run_benchmark("mov 64 (nt)", memcpy::memcpy_mov_64_nt);
     run_benchmark("mov 128 (nt)", memcpy::memcpy_mov_128_nt);
     #[cfg(target_feature = "avx")]
     run_benchmark("mov 256 (nt)", memcpy::memcpy_mov_256_nt);
+    run_benchmark("mov 64 (nt+pl)", memcpy::memcpy_mov_64_nt_pl);
+    run_benchmark("mov 128 (nt+pl)", memcpy::memcpy_mov_128_nt_pl);
+    #[cfg(target_feature = "avx")]
+    run_benchmark("mov 256 (nt+pl)", memcpy::memcpy_mov_256_nt_pl);
     run_benchmark("rep movsb", memcpy::memcpy_rep_movsb);
     run_benchmark("rep movsq", memcpy::memcpy_rep_movsq);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,11 @@ pub fn memcpy_mov_32(size: usize, source: *mut u8, destination: *mut u8) {
             "    inc {counter:r}",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size / 4,
+            temp = out(reg) _,
             options(nostack),
         );
     }
@@ -28,11 +28,11 @@ pub fn memcpy_mov_64(size: usize, source: *mut u8, destination: *mut u8) {
             "    inc {counter:r}",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size / 8,
+            temp = out(reg) _,
             options(nostack),
         );
     }
@@ -41,17 +41,18 @@ pub fn memcpy_mov_64(size: usize, source: *mut u8, destination: *mut u8) {
 pub fn memcpy_mov_128(size: usize, source: *mut u8, destination: *mut u8) {
     unsafe {
         asm!(
+            "    vzeroall",
             "2:",
             "    movdqa {temp}, [{source} + {counter:r}]",
             "    movdqa [{destination} + {counter:r}], {temp}",
             "    add {counter:r}, 16",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(xmm_reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size,
+            temp = out(xmm_reg) _,
             options(nostack),
         );
     }
@@ -61,17 +62,160 @@ pub fn memcpy_mov_128(size: usize, source: *mut u8, destination: *mut u8) {
 pub fn memcpy_mov_256(size: usize, source: *mut u8, destination: *mut u8) {
     unsafe {
         asm!(
+            "    vzeroall",
             "2:",
             "    vmovdqa {temp}, [{source} + {counter:r}]",
             "    vmovdqa [{destination} + {counter:r}], {temp}",
             "    add {counter:r}, 32",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(ymm_reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size,
+            temp = out(ymm_reg) _,
+            options(nostack),
+        );
+    }
+}
+
+pub fn memcpy_mov_64_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "2:",
+            "    mov {temp0:r}, [{source} + {counter:r}]",
+            "    mov {temp1:r}, [{source} + {counter:r} + 8]",
+            "    mov {temp2:r}, [{source} + {counter:r} + 16]",
+            "    mov {temp3:r}, [{source} + {counter:r} + 24]",
+            "    mov {temp4:r}, [{source} + {counter:r} + 32]",
+            "    mov {temp5:r}, [{source} + {counter:r} + 40]",
+            "    mov {temp6:r}, [{source} + {counter:r} + 48]",
+            "    mov {temp7:r}, [{source} + {counter:r} + 56]",
+            "    mov [{destination} + {counter:r}], {temp0:r}",
+            "    mov [{destination} + {counter:r} + 8], {temp1:r}",
+            "    mov [{destination} + {counter:r} + 16], {temp2:r}",
+            "    mov [{destination} + {counter:r} + 24], {temp3:r}",
+            "    mov [{destination} + {counter:r} + 32], {temp4:r}",
+            "    mov [{destination} + {counter:r} + 40], {temp5:r}",
+            "    mov [{destination} + {counter:r} + 48], {temp6:r}",
+            "    mov [{destination} + {counter:r} + 56], {temp7:r}",
+            "    add {counter:r}, 64",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            temp0 = out(reg) _,
+            temp1 = out(reg) _,
+            temp2 = out(reg) _,
+            temp3 = out(reg) _,
+            temp4 = out(reg) _,
+            temp5 = out(reg) _,
+            temp6 = out(reg) _,
+            temp7 = out(reg) _,
+            options(nostack),
+        );
+    }
+}
+
+pub fn memcpy_mov_128_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "    vzeroall",
+            "2:",
+            "    movdqa xmm0, [{source} + {counter:r}]",
+            "    movdqa xmm1, [{source} + {counter:r} + 16]",
+            "    movdqa xmm2, [{source} + {counter:r} + 32]",
+            "    movdqa xmm3, [{source} + {counter:r} + 48]",
+            "    movdqa xmm4, [{source} + {counter:r} + 64]",
+            "    movdqa xmm5, [{source} + {counter:r} + 80]",
+            "    movdqa xmm6, [{source} + {counter:r} + 96]",
+            "    movdqa xmm7, [{source} + {counter:r} + 112]",
+            "    movdqa [{destination} + {counter:r}], xmm0",
+            "    movdqa [{destination} + {counter:r} + 16], xmm1",
+            "    movdqa [{destination} + {counter:r} + 32], xmm2",
+            "    movdqa [{destination} + {counter:r} + 48], xmm3",
+            "    movdqa [{destination} + {counter:r} + 64], xmm4",
+            "    movdqa [{destination} + {counter:r} + 80], xmm5",
+            "    movdqa [{destination} + {counter:r} + 96], xmm6",
+            "    movdqa [{destination} + {counter:r} + 112], xmm7",
+            "    add {counter:r}, 128",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            out("xmm0") _,
+            out("xmm1") _,
+            out("xmm2") _,
+            out("xmm3") _,
+            out("xmm4") _,
+            out("xmm5") _,
+            out("xmm6") _,
+            out("xmm7") _,
+            options(nostack),
+        );
+    }
+}
+
+#[cfg(target_feature = "avx")]
+pub fn memcpy_mov_256_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "    vzeroall",
+            "2:",
+            "    vmovdqa ymm0, [{source} + {counter:r}]",
+            "    vmovdqa ymm1, [{source} + {counter:r} + 32]",
+            "    vmovdqa ymm2, [{source} + {counter:r} + 64]",
+            "    vmovdqa ymm3, [{source} + {counter:r} + 96]",
+            "    vmovdqa ymm4, [{source} + {counter:r} + 128]",
+            "    vmovdqa ymm5, [{source} + {counter:r} + 160]",
+            "    vmovdqa ymm6, [{source} + {counter:r} + 192]",
+            "    vmovdqa ymm7, [{source} + {counter:r} + 224]",
+            "    vmovdqa [{destination} + {counter:r}], ymm0",
+            "    vmovdqa [{destination} + {counter:r} + 32], ymm1",
+            "    vmovdqa [{destination} + {counter:r} + 64], ymm2",
+            "    vmovdqa [{destination} + {counter:r} + 96], ymm3",
+            "    vmovdqa [{destination} + {counter:r} + 128], ymm4",
+            "    vmovdqa [{destination} + {counter:r} + 160], ymm5",
+            "    vmovdqa [{destination} + {counter:r} + 192], ymm6",
+            "    vmovdqa [{destination} + {counter:r} + 224], ymm7",
+            "    add {counter:r}, 256",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            // out("ymm0") _,
+            // out("ymm1") _,
+            // out("ymm2") _,
+            // out("ymm3") _,
+            // out("ymm4") _,
+            // out("ymm5") _,
+            // out("ymm6") _,
+            // out("ymm7") _,
+            options(nostack),
+        );
+    }
+}
+
+pub fn memcpy_mov_64_nt(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "2:",
+            "    mov {temp:r}, [{source} + {counter:r} * 8]",
+            "    movnti [{destination} + {counter:r} * 8], {temp:r}",
+            "    inc {counter:r}",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size / 8,
+            temp = out(reg) _,
             options(nostack),
         );
     }
@@ -80,17 +224,18 @@ pub fn memcpy_mov_256(size: usize, source: *mut u8, destination: *mut u8) {
 pub fn memcpy_mov_128_nt(size: usize, source: *mut u8, destination: *mut u8) {
     unsafe {
         asm!(
+            "    vzeroall",
             "2:",
             "    movdqa {temp}, [{source} + {counter:r}]",
             "    movntdq [{destination} + {counter:r}], {temp}",
             "    add {counter:r}, 16",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(xmm_reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size,
+            temp = out(xmm_reg) _,
             options(nostack),
         );
     }
@@ -100,23 +245,148 @@ pub fn memcpy_mov_128_nt(size: usize, source: *mut u8, destination: *mut u8) {
 pub fn memcpy_mov_256_nt(size: usize, source: *mut u8, destination: *mut u8) {
     unsafe {
         asm!(
+            "    vzeroall",
             "2:",
             "    vmovdqa {temp}, [{source} + {counter:r}]",
             "    vmovntdq [{destination} + {counter:r}], {temp}",
             "    add {counter:r}, 32",
             "    cmp {counter:r}, {size}",
             "    jne 2b",
-            temp = out(ymm_reg) _,
             source = in(reg) source,
             destination = in(reg) destination,
             counter = inout(reg) 0 => _,
             size = in(reg) size,
+            temp = out(ymm_reg) _,
             options(nostack),
         );
     }
 }
 
-pub fn memcpy_rep_movsb(mut _size: usize, source: *mut u8, destination: *mut u8) {
+pub fn memcpy_mov_64_nt_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "2:",
+            "    mov {temp0:r}, [{source} + {counter:r}]",
+            "    mov {temp1:r}, [{source} + {counter:r} + 8]",
+            "    mov {temp2:r}, [{source} + {counter:r} + 16]",
+            "    mov {temp3:r}, [{source} + {counter:r} + 24]",
+            "    mov {temp4:r}, [{source} + {counter:r} + 32]",
+            "    mov {temp5:r}, [{source} + {counter:r} + 40]",
+            "    mov {temp6:r}, [{source} + {counter:r} + 48]",
+            "    mov {temp7:r}, [{source} + {counter:r} + 56]",
+            "    movnti [{destination} + {counter:r}], {temp0:r}",
+            "    movnti [{destination} + {counter:r} + 8], {temp1:r}",
+            "    movnti [{destination} + {counter:r} + 16], {temp2:r}",
+            "    movnti [{destination} + {counter:r} + 24], {temp3:r}",
+            "    movnti [{destination} + {counter:r} + 32], {temp4:r}",
+            "    movnti [{destination} + {counter:r} + 40], {temp5:r}",
+            "    movnti [{destination} + {counter:r} + 48], {temp6:r}",
+            "    movnti [{destination} + {counter:r} + 56], {temp7:r}",
+            "    add {counter:r}, 64",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            temp0 = out(reg) _,
+            temp1 = out(reg) _,
+            temp2 = out(reg) _,
+            temp3 = out(reg) _,
+            temp4 = out(reg) _,
+            temp5 = out(reg) _,
+            temp6 = out(reg) _,
+            temp7 = out(reg) _,
+            source = in(reg) source,
+            options(nostack),
+        );
+    }
+}
+
+pub fn memcpy_mov_128_nt_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "    vzeroall",
+            "2:",
+            "    movdqa xmm0, [{source} + {counter:r}]",
+            "    movdqa xmm1, [{source} + {counter:r} + 16]",
+            "    movdqa xmm2, [{source} + {counter:r} + 32]",
+            "    movdqa xmm3, [{source} + {counter:r} + 48]",
+            "    movdqa xmm4, [{source} + {counter:r} + 64]",
+            "    movdqa xmm5, [{source} + {counter:r} + 80]",
+            "    movdqa xmm6, [{source} + {counter:r} + 96]",
+            "    movdqa xmm7, [{source} + {counter:r} + 112]",
+            "    movntdq [{destination} + {counter:r}], xmm0",
+            "    movntdq [{destination} + {counter:r} + 16], xmm1",
+            "    movntdq [{destination} + {counter:r} + 32], xmm2",
+            "    movntdq [{destination} + {counter:r} + 48], xmm3",
+            "    movntdq [{destination} + {counter:r} + 64], xmm4",
+            "    movntdq [{destination} + {counter:r} + 80], xmm5",
+            "    movntdq [{destination} + {counter:r} + 96], xmm6",
+            "    movntdq [{destination} + {counter:r} + 112], xmm7",
+            "    add {counter:r}, 128",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            out("xmm0") _,
+            out("xmm1") _,
+            out("xmm2") _,
+            out("xmm3") _,
+            out("xmm4") _,
+            out("xmm5") _,
+            out("xmm6") _,
+            out("xmm7") _,
+            options(nostack),
+        );
+    }
+}
+
+#[cfg(target_feature = "avx")]
+pub fn memcpy_mov_256_nt_pl(size: usize, source: *mut u8, destination: *mut u8) {
+    unsafe {
+        asm!(
+            "    vzeroall",
+            "2:",
+            "    vmovdqa ymm0, [{source} + {counter:r}]",
+            "    vmovdqa ymm1, [{source} + {counter:r} + 32]",
+            "    vmovdqa ymm2, [{source} + {counter:r} + 64]",
+            "    vmovdqa ymm3, [{source} + {counter:r} + 96]",
+            "    vmovdqa ymm4, [{source} + {counter:r} + 128]",
+            "    vmovdqa ymm5, [{source} + {counter:r} + 160]",
+            "    vmovdqa ymm6, [{source} + {counter:r} + 192]",
+            "    vmovdqa ymm7, [{source} + {counter:r} + 224]",
+            "    vmovntdq [{destination} + {counter:r}], ymm0",
+            "    vmovntdq [{destination} + {counter:r} + 32], ymm1",
+            "    vmovntdq [{destination} + {counter:r} + 64], ymm2",
+            "    vmovntdq [{destination} + {counter:r} + 96], ymm3",
+            "    vmovntdq [{destination} + {counter:r} + 128], ymm4",
+            "    vmovntdq [{destination} + {counter:r} + 160], ymm5",
+            "    vmovntdq [{destination} + {counter:r} + 192], ymm6",
+            "    vmovntdq [{destination} + {counter:r} + 224], ymm7",
+            "    add {counter:r}, 256",
+            "    cmp {counter:r}, {size}",
+            "    jne 2b",
+            source = in(reg) source,
+            destination = in(reg) destination,
+            counter = inout(reg) 0 => _,
+            size = in(reg) size,
+            // out("ymm0") _,
+            // out("ymm1") _,
+            // out("ymm2") _,
+            // out("ymm3") _,
+            // out("ymm4") _,
+            // out("ymm5") _,
+            // out("ymm6") _,
+            // out("ymm7") _,
+            options(nostack),
+        );
+    }
+}
+
+pub fn memcpy_rep_movsb(size: usize, source: *mut u8, destination: *mut u8) {
+    let mut _size = size;
     unsafe {
         asm!(
             "rep movsb",
@@ -128,8 +398,8 @@ pub fn memcpy_rep_movsb(mut _size: usize, source: *mut u8, destination: *mut u8)
     }
 }
 
-pub fn memcpy_rep_movsq(mut _size: usize, source: *mut u8, destination: *mut u8) {
-    _size /= 8;
+pub fn memcpy_rep_movsq(size: usize, source: *mut u8, destination: *mut u8) {
+    let mut _size = size / 8;
     unsafe {
         asm!(
             "rep movsq",
